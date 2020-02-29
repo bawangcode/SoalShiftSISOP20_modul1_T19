431621ced1cd899d162640e52520d3440f9ee838
bawangcode <49397891+bawangcode@users.noreply.github.com>
2020-02-29 09:02:28 +0700

    Update Soal3cfix.sh

--- soal3/Soal3cfix.sh
+++ soal3/Soal3cfix.sh
@@ -1,30 +1,37 @@
 #!bin/bash
 
 cekdir=`ls -d kenangan | wc -l`
-if [ cekdir == 0 ]
+if [ $cekdir == 0 ]
 then
   mkdir kenangan
 fi
+
+
 cekdir2=`ls -d duplicate | wc -l`
-if [ cekdir2 == 0 ]
+if [ $cekdir2 == 0 ]
 then
   mkdir duplicate
 fi
 
 sum=`ls -d pdkt_kusuma_* | wc -l`
+
 for ((i=1; i<=sum; i=i+1))
 do
-  j=$sum - $i
+  j=`expr $sum - $i`
   img=`awk  -F '/' '/Location/ {print substr($4,7,21)}' wget.log | head -n $i | tail -n 1`
-  logdinamis=`awk -F '/' '/Location/ {print substr($4,7,21)}' wget.log | tail -n $j`
-  count=`echo $logdinamis | awk '/$img/ {++n} END {print n}'`
+  count=`awk -F '/' '/Location/ {print substr($4,7,21)}' wget.log | tail -n $j | awk -v img=$img 'BEGIN {n=0} /'$img'/ {++n} END {print n}'`
 
-  if [ $count == 0 ]
+if [ $count == 0 ]
   then
-    mv pdkt_kusuma_$i kenangan/kenangan_$i
+    countk=`ls kenangan/kenangan_* | wc -l`
+    k=`expr 1 + $countk`
+    mv pdkt_kusuma_$i kenangan/kenangan_$k
   else
-    mv pdkt_kusuma_$i duplicate/duplicate_$i
+    countd=`ls duplicate/duplicate_* | wc -l`
+    d=`expr $countd + 1`
+    mv pdkt_kusuma_$i duplicate/duplicate_$d
   fi
+
 done
 
 cat wget.log >> wget.log.bak
